a418aff791f55db9624990b5f0b423e947b8fbe5
Javier Santo Domingo <j-a-s-d@coderesearchlabs.com>
2021-02-26 17:22:25 -0300

    adding more changes for 0.3.3

--- tests/test_xam.nim
+++ tests/test_xam.nim
@@ -39,10 +39,13 @@ suite "test xam":
 
   test "test sandboxed":
     check(sandboxed(nothing))
+    check(not sandboxed(nil))
     check(not sandboxed(failure))
     var msg = ""
     discard sandboxed(failure, proc (error: string) = (msg = error))
     check(msg == "Blah")
+    discard sandboxed(nil, proc (error: string) = (msg = error))
+    check(msg == "Cannot invoke nil")
 
   test "test silent":
     var msg = ""
@@ -56,6 +59,10 @@ suite "test xam":
     check(err.msg == "Blah")
     catch(nothing, err)
     check(err == nil)
+    catch(nothing, err)
+    check(err == nil)
+    catch(nil, err)
+    check(err.msg == "Cannot invoke nil")
 
   # "suite teardown: run once after the tests"
 
